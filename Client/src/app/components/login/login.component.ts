d2935f163fda815e30d75dd6f903b056bd2dc8c1
oritvadai <oritvadai@gmail.com>
2020-06-23 20:47:06 +0300

    Client - login & header UI changes

--- Client/src/app/components/login/login.component.ts
+++ Client/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/app/models/user';
 import { Router } from '@angular/router';
@@ -10,17 +10,31 @@ import { store } from 'src/app/redux/store';
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+    public credentials = new User();
     public user = new User();
+    public hasToken: boolean;
+    public unsubscribe: Function;
 
     constructor(private authService: AuthService, private router: Router) { }
 
+    ngOnInit(): void {
+
+        this.unsubscribe = store.subscribe(() => {
+            this.user = store.getState().user;
+            this.hasToken = store.getState().hasToken;
+        });
+
+        this.user = store.getState().user;
+        this.hasToken = store.getState().hasToken;
+    }
+
     login() {
-        if (this.user.username && this.user.password) {
+        if (this.credentials.username && this.credentials.password) {
 
             this.authService
-                .login(this.user.username, this.user.password)
+                .login(this.credentials.username, this.credentials.password)
                 .subscribe(response => {
                     sessionStorage.setItem("user", JSON.stringify(response.user));
                     sessionStorage.setItem("token", response.token);
@@ -28,13 +42,17 @@ export class LoginComponent {
                     const action = { type: ActionType.Login, payload: response.user };
                     store.dispatch(action);
 
-                    if (response.user.role == "user") {
-                        this.router.navigateByUrl("/store");
-                    } else if (response.user.role == "admin") {
-                        this.router.navigateByUrl("/admin");
-                    }
+                    // if (response.user.role == "user") {
+                    //     this.router.navigateByUrl("/store");
+                    // } else if (response.user.role == "admin") {
+                    //     this.router.navigateByUrl("/admin");
+                    // }
                 },
                     err => alert(err.message));
         }
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe();
     }
 }
