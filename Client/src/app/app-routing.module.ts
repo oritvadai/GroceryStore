6df8c67f25264f4e218ad4a5a299664e5ddb11bf
oritvadai <oritvadai@gmail.com>
2020-06-27 15:50:53 +0300

    Client - Page Not Found Routing

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -8,26 +8,26 @@ import { AddProductComponent } from './components/add-product/add-product.compon
 import { EditProductComponent } from './components/edit-product/edit-product.component';
 import { StoreComponent } from './components/store/store.component';
 import { OrderComponent } from './components/order/order.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "store", component: StoreComponent },
-  { path: "store/:categoryId", component: StoreComponent },
+    { path: "home", component: HomeComponent },
+    { path: "register", component: RegisterComponent },
+    { path: "store", component: StoreComponent },
+    { path: "store/:categoryId", component: StoreComponent },
 
-  { path: "order", component: OrderComponent },
-  { path: "admin", component: AdminComponent },
-  { path: "add-product", component: AddProductComponent },
-  { path: "edit-product/:productId", component: EditProductComponent },
-  { path: "logout", component: LogoutComponent },
-  { path: "", redirectTo: "/home", pathMatch: "full" },
-  // { path: "**", component: PageNotFoundComponent }
-
+    { path: "order", component: OrderComponent },
+    { path: "admin", component: AdminComponent },
+    { path: "add-product", component: AddProductComponent },
+    { path: "edit-product/:productId", component: EditProductComponent },
+    { path: "logout", component: LogoutComponent },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
