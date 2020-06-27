7bbe47bbb0e1edda2a3bfcac50fc48dc23da54ff
oritvadai <oritvadai@gmail.com>
2020-06-27 15:53:24 +0300

    Client
    Header Redux fixes
    Login ui changes

--- Client/src/app/components/header/header.component.ts
+++ Client/src/app/components/header/header.component.ts
@@ -3,23 +3,36 @@ import { User } from 'src/app/models/user';
 import { store } from 'src/app/redux/store';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent implements OnInit {
 
     public user = new User();
     public hasToken: boolean;
+    public unsubscribe: Function;
 
-	constructor() { }
+    constructor() { }
 
-	ngOnInit(): void {
+    ngOnInit(): void {
 
-		store.subscribe(() => {
+        this.unsubscribe = store.subscribe(() => {
             this.user = store.getState().user;
             this.hasToken = store.getState().hasToken;
         });
-	}
+
+        this.user = store.getState().user;
+        this.hasToken = store.getState().hasToken;
+
+        // if (!this.user || !this.hasToken) {
+        //     this.user = JSON.parse(sessionStorage.getItem("user"))
+        //     this.hasToken = sessionStorage.get("token");
+        // }
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe();
+    }
 }
