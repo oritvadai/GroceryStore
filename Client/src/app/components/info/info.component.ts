f086e77ce9cd006a79c5c92219341a83aa360037
oritvadai <oritvadai@gmail.com>
2020-07-02 19:00:25 +0300

    Client - info shows user last order

--- Client/src/app/components/info/info.component.ts
+++ Client/src/app/components/info/info.component.ts
@@ -4,6 +4,8 @@ import { GroceryService } from 'src/app/services/grocery.service';
 import { Order } from 'src/app/models/order';
 import { ActionType } from 'src/app/redux/action-type';
 import { store } from 'src/app/redux/store';
+import { User } from 'src/app/models/user';
+import { Cart } from 'src/app/models/cart';
 
 @Component({
     selector: 'app-info',
@@ -16,7 +18,10 @@ export class InfoComponent implements OnInit {
 
     public productsNum: number;
     public ordersNum: number;
-
+    
+    public user = new User();
+    public hasToken: boolean;
+    public lastOrder = new Date();
 
     constructor(private groceryService: GroceryService) { }
 
@@ -25,10 +30,17 @@ export class InfoComponent implements OnInit {
         this.unsubscribe = store.subscribe(() => {
             this.productsNum = store.getState().productsNum;
             this.ordersNum = store.getState().ordersNum;
+            this.user = store.getState().user;
+            this.hasToken = store.getState().hasToken;
+            // this.cart = store.getState().cart;
         });
 
-        if (store.getState().productsNum === 0) {
+        const user = store.getState().user;
 
+        this.user = store.getState().user;
+        this.hasToken = store.getState().hasToken;
+
+        if (store.getState().productsNum === 0) {
             this.groceryService
                 .getNumProducts()
                 .subscribe(productsNum => {
@@ -51,6 +63,22 @@ export class InfoComponent implements OnInit {
         } else {
             this.productsNum = store.getState().productsNum;
             this.ordersNum = store.getState().ordersNum;
+        }
+
+        if (!store.getState().lastOrder) {
+            this.groceryService
+                .getLastOrderByUser(user._id)
+                .subscribe(lastOrder => {
+                    console.log(lastOrder);
+                    this.lastOrder = lastOrder.orderDate;
+
+                    const action = { type: ActionType.GetLastOrderByUser, payload: this.lastOrder };
+                    store.dispatch(action);
+
+                }, err => alert(err.message));
+        }
+        else {
+            this.lastOrder = store.getState().lastOrder;
         }
     }
 
