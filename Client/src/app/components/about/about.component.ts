dfd11cf52f35a3dee8dd9bf70dc13b5883d4924b
oritvadai <oritvadai@gmail.com>
2020-07-12 15:31:33 +0300

    Client - about + picture

--- Client/src/app/components/about/about.component.ts
+++ Client/src/app/components/about/about.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+    public url: string;
+    public picFileName: string;
+
+    ngOnInit(): void {
+        
+    }
 }
